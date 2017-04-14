87dc6f21537b5af072e1f9377cdd55420e5e4327
Jorge Atempa <atempaitm@gmail.com>
2017-04-13 20:18:04 -0700

    Agregar cabeceras para evadir CORS

--- web/app_dev.php
+++ web/app_dev.php
@@ -18,6 +18,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
