0bf0ceec2590634a90055259f8f67c81cdf1c6d5
Jorge Atempa <atempaitm@gmail.com>
2017-04-14 19:41:36 -0700

    Deshabilitar datos de prueba de Alumnos

--- src/AppBundle/DataFixtures/Alice/FixtureSet.php
+++ src/AppBundle/DataFixtures/Alice/FixtureSet.php
@@ -5,6 +5,6 @@ $manager = $this->getContainer()->get('h4cc_alice_fixtures.manager');
 // Get a FixtureSet with __default__ options.
 $set = $manager->createFixtureSet();
 $set->addFile(__DIR__.'/Carreras.yml', 'yaml');
-$set->addFile(__DIR__.'/Alumnos.yml', 'yaml');
+//$set->addFile(__DIR__.'/Alumnos.yml', 'yaml');
 
 return $set;
