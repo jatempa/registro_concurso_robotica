117a3a07e681ad3d92b1e3ad6a92832479811b43
Jorge Atempa <atempaitm@gmail.com>
2017-04-14 21:05:05 -0700

    Crear datos de prueba para Asesores

--- src/AppBundle/DataFixtures/Alice/FixtureSet.php
+++ src/AppBundle/DataFixtures/Alice/FixtureSet.php
@@ -4,7 +4,9 @@ $manager = $this->getContainer()->get('h4cc_alice_fixtures.manager');
 
 // Get a FixtureSet with __default__ options.
 $set = $manager->createFixtureSet();
+$set->addFile(__DIR__.'/Usuarios.yml', 'yaml');
 $set->addFile(__DIR__.'/Carreras.yml', 'yaml');
+$set->addFile(__DIR__.'/Asesores.yml', 'yaml');
 //$set->addFile(__DIR__.'/Alumnos.yml', 'yaml');
 
 return $set;
