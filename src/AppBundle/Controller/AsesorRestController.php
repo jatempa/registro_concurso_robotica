ed2fcf69ae217d5c954120943d3430a4a880a437
Jorge Atempa <atempaitm@gmail.com>
2017-07-13 15:29:08 -0700

    Separar la lógica de la aplicación en diferentes controladores

--- src/AppBundle/Controller/AsesorRestController.php
+++ src/AppBundle/Controller/AsesorRestController.php
@@ -6,22 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\View\View;
 
-class RoboticaRestController extends Controller
+class AsesorRestController extends Controller
 {
-    /**
-     * @Get("/carreras")
-     */
-    public function getCarrerasAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $carreras = $em->getRepository('AppBundle:Carrera')->findAll();
-
-        $view = View::create()->setData(array('carreras' => $carreras));
-
-        return $this->get('fos_rest.view_handler')->handle($view);
-    }
-
     /**
      * @Get("/asesores")
      */
---
+++ src/AppBundle/Controller/CarreraRestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\View\View;
+
+class CarreraRestController extends Controller
+{
+    /**
+     * @Get("/carreras")
+     */
+    public function getCarrerasAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $carreras = $em->getRepository('AppBundle:Carrera')->findAll();
+
+        $view = View::create()->setData(array('carreras' => $carreras));
+
+        return $this->get('fos_rest.view_handler')->handle($view);
+    }
+}
