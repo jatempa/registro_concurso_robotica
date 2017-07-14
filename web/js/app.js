293de23ef59f3e84d733fb95640b52f940287a2d
Jorge Atempa <atempaitm@gmail.com>
2017-07-13 23:50:01 -0700

    Crear una plantilla base para el formulario de registro de equipos, utilizando Bootstrap + VueJS

---
+++ web/js/app.js
@@ -0,0 +1,35 @@
+
+new Vue({
+    delimiters: ['${', '}'],
+    el: 'main',
+    data () {
+        return {
+            asesores: [],
+            carreras: [],
+            nuevoasesor: false
+        }
+    },
+    mounted () {
+        this.fetchAsesores(),
+        this.fetchCarreras()
+    },
+    methods: {
+      fetchAsesores () {
+        axios.get('/api/asesores')
+             .then(response => {
+                this.asesores = response.data.asesores
+             }
+        )
+      },
+      fetchCarreras () {
+        axios.get('/api/carreras')
+             .then(response => {
+                this.carreras = response.data.carreras
+             }
+        )
+      },
+      selectAsesor () {
+        this.nuevoasesor = false
+      }
+    }
+})
