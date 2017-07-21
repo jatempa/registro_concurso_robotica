c9bc5a026c253b8a30fa3a82d2b7eb2ec524f7af
Jorge Atempa <atempaitm@gmail.com>
2017-07-21 11:37:43 -0700

    Validar variables en el servidor

--- src/AppBundle/Controller/EquipoRestController.php
+++ src/AppBundle/Controller/EquipoRestController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Alumno;
+use AppBundle\Entity\Asesor;
 use AppBundle\Entity\Equipo;
 use AppBundle\Entity\Robot;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -33,66 +34,89 @@ class EquipoRestController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->getConnection()->beginTransaction(); // suspend auto-commit
             try {
-                // Create Robot
-                $robot = new Robot();
-                $robot->setNombre($robotName);
-                $em->persist($robot);
-                $em->flush();
-                // Get Asesor
-                $asesor = $em->getRepository('AppBundle:Asesor')->findOneById($asesorId);
-                // Create Captain
-                $captain = new Alumno();
-                $captain->setNoControl($captainObj['noControl']);
-                $captain->setNombre($captainObj['name']);
-                $captain->setApellidoPaterno($captainObj['firstLastName']);
-                $captain->setApellidoMaterno($captainObj['secondLastName']);
-                $captain->setCorreoElectronico($captainObj['email']);
-                $captain->setSemestre($captainObj['semester']);
-                $captain->setCapitan(true);
-                // Get Career
-                $carrera = $em->getRepository('AppBundle:Carrera')->findOneById($captainObj['selectedCareer']);
-                // Set Career
-                $captain->setCarrera($carrera);
-                $em->persist($captain);
-                $em->flush();
-                // Create First Alumn
-                $firstAlumn = new Alumno();
-                $firstAlumn->setNoControl($firstAlumnObj['noControl']);
-                $firstAlumn->setNombre($firstAlumnObj['name']);
-                $firstAlumn->setApellidoPaterno($firstAlumnObj['firstLastName']);
-                $firstAlumn->setApellidoMaterno($firstAlumnObj['secondLastName']);
-                $firstAlumn->setCorreoElectronico($firstAlumnObj['email']);
-                $firstAlumn->setSemestre($firstAlumnObj['semester']);
-                $firstAlumn->setCapitan(false);
-                // Get Career
-                $carrera = $em->getRepository('AppBundle:Carrera')->findOneById($firstAlumnObj['selectedCareer']);
-                // Set Career
-                $firstAlumn->setCarrera($carrera);
-                $em->persist($firstAlumn);
-                $em->flush();
-                // Create Second Alumn
-                $secondAlumn = new Alumno();
-                $secondAlumn->setNoControl($secondAlumnObj['noControl']);
-                $secondAlumn->setNombre($secondAlumnObj['name']);
-                $secondAlumn->setApellidoPaterno($secondAlumnObj['firstLastName']);
-                $secondAlumn->setApellidoMaterno($secondAlumnObj['secondLastName']);
-                $secondAlumn->setCorreoElectronico($secondAlumnObj['email']);
-                $secondAlumn->setSemestre($secondAlumnObj['semester']);
-                $secondAlumn->setCapitan(false);
-                // Get Career
-                $carrera = $em->getRepository('AppBundle:Carrera')->findOneById($secondAlumnObj['selectedCareer']);
-                // Set Career
-                $secondAlumn->setCarrera($carrera);
-                $em->persist($secondAlumn);
-                $em->flush();
-                // Save Team
                 $team = new Equipo();
                 $team->setNombre($teamName);
-                $team->setRobot($robot);
+
+                if(!is_null($robotName)) {
+                    // Create Robot
+                    $robot = new Robot();
+                    $robot->setNombre($robotName);
+                    $em->persist($robot);
+                    $em->flush();
+                    $team->setRobot($robot);
+                }
+
+                // Get Asesor
+                if (!is_null($asesorId['name']) && !is_null($asesorId['firstLastName']) && !is_null($asesorId['email'])) {
+                    $asesor = new Asesor();
+                    $asesor->setNombre($asesorId['name']);
+                    $asesor->setApellidoPaterno($asesorId['firstLastName']);
+                    $asesor->setApellidoMaterno($asesorId['secondLastName']);
+                    $asesor->setCorreoElectronico($asesorId['email']);
+                    $em->persist($asesor);
+                    $em->flush();
+                } else if ($asesorId > 0) {
+                    $asesor = $em->getRepository('AppBundle:Asesor')->findOneById($asesorId);
+                }
                 $team->setAsesor($asesor);
-                $team->addAlumno($captain);
-                $team->addAlumno($firstAlumn);
-                $team->addAlumno($secondAlumn);
+
+                if (!is_null($captainObj)) {
+                    // Create Captain
+                    $captain = new Alumno();
+                    $captain->setNoControl($captainObj['noControl']);
+                    $captain->setNombre($captainObj['name']);
+                    $captain->setApellidoPaterno($captainObj['firstLastName']);
+                    $captain->setApellidoMaterno($captainObj['secondLastName']);
+                    $captain->setCorreoElectronico($captainObj['email']);
+                    $captain->setSemestre($captainObj['semester']);
+                    $captain->setCapitan(true);
+                    // Get Career
+                    $carrera = $em->getRepository('AppBundle:Carrera')->findOneById($captainObj['selectedCareer']);
+                    // Set Career
+                    $captain->setCarrera($carrera);
+                    $em->persist($captain);
+                    $em->flush();
+                    $team->addAlumno($captain);
+                }
+
+                if (!is_null($firstAlumnObj)) {
+                    // Create First Alumn
+                    $firstAlumn = new Alumno();
+                    $firstAlumn->setNoControl($firstAlumnObj['noControl']);
+                    $firstAlumn->setNombre($firstAlumnObj['name']);
+                    $firstAlumn->setApellidoPaterno($firstAlumnObj['firstLastName']);
+                    $firstAlumn->setApellidoMaterno($firstAlumnObj['secondLastName']);
+                    $firstAlumn->setCorreoElectronico($firstAlumnObj['email']);
+                    $firstAlumn->setSemestre($firstAlumnObj['semester']);
+                    $firstAlumn->setCapitan(false);
+                    // Get Career
+                    $carrera = $em->getRepository('AppBundle:Carrera')->findOneById($firstAlumnObj['selectedCareer']);
+                    // Set Career
+                    $firstAlumn->setCarrera($carrera);
+                    $em->persist($firstAlumn);
+                    $em->flush();
+                    $team->addAlumno($firstAlumn);
+                }
+
+                if (!is_null($secondAlumnObj)) {
+                    // Create Second Alumn
+                    $secondAlumn = new Alumno();
+                    $secondAlumn->setNoControl($secondAlumnObj['noControl']);
+                    $secondAlumn->setNombre($secondAlumnObj['name']);
+                    $secondAlumn->setApellidoPaterno($secondAlumnObj['firstLastName']);
+                    $secondAlumn->setApellidoMaterno($secondAlumnObj['secondLastName']);
+                    $secondAlumn->setCorreoElectronico($secondAlumnObj['email']);
+                    $secondAlumn->setSemestre($secondAlumnObj['semester']);
+                    $secondAlumn->setCapitan(false);
+                    // Get Career
+                    $carrera = $em->getRepository('AppBundle:Carrera')->findOneById($secondAlumnObj['selectedCareer']);
+                    // Set Career
+                    $secondAlumn->setCarrera($carrera);
+                    $em->persist($secondAlumn);
+                    $em->flush();
+                    $team->addAlumno($secondAlumn);
+                }
+                // Save Team
                 $em->persist($team);
                 $em->flush();
                 $em->getConnection()->commit();
--- web/js/app.js
+++ web/js/app.js
@@ -6,14 +6,14 @@ new Vue({
             asesors: [],
             careers: [],
             newAsesor: false,
-            robot: '',
-            team: '',
+            robot: null,
+            team: null,
             selectedAsesor: 0,
             asesor: {
-                name: '',
-                firstLastName: '',
-                secondLastName: '',
-                email: ''
+                name: null,
+                firstLastName: null,
+                secondLastName: null,
+                email: null
             },
             captain: {
                 noControl: '',
@@ -67,12 +67,21 @@ new Vue({
           axios.defaults.headers.common = {
               'X-Requested-With': 'XMLHttpRequest',
           };
+          let asesor, captain, firstAlumn, secondAlumn = null;
 
-          let teamData = {
-              team: this.team,
-              robot: this.robot,
-              asesor: this.selectedAsesor,
-              captain: {
+          if ((this.selectedAsesor === 0) && (this.asesor.name !== '' || this.asesor.name !== null) && (this.asesor.firstLastName !== '' || this.asesor.firstLastName !== null) && (this.asesor.email !== '' || this.asesor.email !== null)) {
+              asesor = {
+                  name: this.asesor.name,
+                  firstLastName: this.asesor.firstLastName,
+                  secondLastName: this.asesor.secondLastName,
+                  email: this.asesor.email
+              };
+          } else if (this.selectedAsesor > 0){
+              asesor = this.selectedAsesor;
+          }
+
+          if ((this.captain.noControl !== '' || this.captain.noControl !== null) && (this.captain.name !== '' || this.captain.name !== null) && (this.captain.firstLastName !== '' || this.captain.firstLastName !== null) && (this.captain.email !== '' || this.captain.email !== null) && (this.captain.selectedCareer > 0) && (parseInt(this.captain.semester) > 0)) {
+              captain = {
                   noControl: this.captain.noControl,
                   name: this.captain.name,
                   firstLastName: this.captain.firstLastName,
@@ -80,8 +89,11 @@ new Vue({
                   email: this.captain.email,
                   selectedCareer: this.captain.selectedCareer,
                   semester: parseInt(this.captain.semester)
-              },
-              firstAlumn: {
+              }
+          }
+
+          if ((this.firstAlumn.noControl !== '' || this.firstAlumn.noControl !== null) && (this.firstAlumn.name !== '' || this.firstAlumn.name !== null) && (this.firstAlumn.firstLastName !== '' || this.firstAlumn.firstLastName !== null) && (this.firstAlumn.email !== '' || this.firstAlumn.email !== null) && (this.firstAlumn.selectedCareer > 0) && (parseInt(this.firstAlumn.semester) > 0)) {
+              firstAlumn = {
                   noControl: this.firstAlumn.noControl,
                   name: this.firstAlumn.name,
                   firstLastName: this.firstAlumn.firstLastName,
@@ -89,8 +101,11 @@ new Vue({
                   email: this.firstAlumn.email,
                   selectedCareer: this.firstAlumn.selectedCareer,
                   semester: parseInt(this.firstAlumn.semester)
-              },
-              secondAlumn: {
+              }
+          }
+
+          if ((this.secondAlumn.noControl !== '' || this.secondAlumn.noControl !== null) && (this.secondAlumn.name !== '' || this.secondAlumn.name !== null) && (this.secondAlumn.firstLastName !== '' || this.secondAlumn.firstLastName !== null) && (this.secondAlumn.email !== '' || this.secondAlumn.email !== null) && (this.secondAlumn.selectedCareer > 0) && (parseInt(this.secondAlumn.semester) > 0)) {
+              secondAlumn = {
                   noControl: this.secondAlumn.noControl,
                   name: this.secondAlumn.name,
                   firstLastName: this.secondAlumn.firstLastName,
@@ -99,18 +114,68 @@ new Vue({
                   selectedCareer: this.secondAlumn.selectedCareer,
                   semester: parseInt(this.secondAlumn.semester)
               }
-          };
+          }
 
+          let teamData = {
+              team: this.team,
+              robot: this.robot,
+              asesor,
+              captain,
+              firstAlumn,
+              secondAlumn
+          };
+          
           axios.post('/api/registro/equipo/nuevo', teamData)
               .then(function (response) {
-                  console.log(response);
+                  console.log(response)
               })
               .catch(function (error) {
                   console.log(error);
               });
+          // Clean form
+          this.clearForm();
+          if(this.newAsesor) {
+              this.fetchAsesors();
+              this.newAsesor = false;
+          }
       },
-      selectAsesor () {
-        this.newAsesor = false
+      selectAddNewAsesor () {
+        this.newAsesor = true;
+        this.selectedAsesor = 0;
+      },
+      unSelectAddNewAsesor () {
+        this.newAsesor = false;
+        this.selectedAsesor = 0;
+      },
+      clearForm () {
+        this.team = '';
+        this.robot = '';
+        this.selectedAsesor = 0;
+        this.asesor.name = null;
+        this.asesor.firstLastName = null;
+        this.asesor.secondLastName = null;
+        this.asesor.email = null;
+        this.captain.noControl = '';
+        this.captain.name = '';
+        this.captain.firstLastName = '';
+        this.captain.secondLastName = '';
+        this.captain.email = '';
+        this.captain.selectedCareer = 0;
+        this.captain.semester = 0;
+        this.firstAlumn.noControl = '';
+        this.firstAlumn.name = '';
+        this.firstAlumn.firstLastName = '';
+        this.firstAlumn.secondLastName = '';
+        this.firstAlumn.email = '';
+        this.firstAlumn.selectedCareer = 0;
+        this.firstAlumn.semester = 0;
+        this.secondAlumn.noControl = '';
+        this.secondAlumn.name = '';
+        this.secondAlumn.firstLastName = '';
+        this.secondAlumn.secondLastName = '';
+        this.secondAlumn.email = '';
+        this.secondAlumn.selectedCareer = 0;
+        this.secondAlumn.semester = 0;
       }
     }
 });
